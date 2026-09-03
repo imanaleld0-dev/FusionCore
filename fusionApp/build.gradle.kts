759f6a23c6ab62e591e792301bc673facf275d30
Twix234 <eldiarimanaliev220@gmail.com>
2026-09-03 08:06:22 +0600

    Add Google Play services dependencies

--- fusionApp/build.gradle.kts
+++ fusionApp/build.gradle.kts
@@ -12,6 +12,8 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.3.0")
     implementation("com.google.android.material:material:1.14.0")
     implementation("com.google.protobuf:protobuf-javalite:4.35.1")
+    implementation("com.google.android.gms:play-services-games:23.1.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation(files(pineAar))
 }
 
