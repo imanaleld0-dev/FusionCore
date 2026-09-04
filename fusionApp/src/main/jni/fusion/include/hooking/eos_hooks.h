3a8ceb01a095b883b1a967d29ddc5424f1232db1
Twix234 <eldiarimanaliev220@gmail.com>
2026-09-04 15:52:15 +0600

    Update eos_hooks.h

--- fusionApp/src/main/jni/fusion/include/hooking/eos_hooks.h
+++ fusionApp/src/main/jni/fusion/include/hooking/eos_hooks.h
@@ -10,13 +10,15 @@
 void install_eos_hooks();
 
 struct CredentialsInternal {
-    int32_t ApiVersion;   // +0x00
-    void*   Token;        // +0x08
-    int32_t Type;         // +0x10
+    int32_t ApiVersion; // +0x00
+    uint32_t padding;   // +0x04
+    void* Token;        // +0x08
+    int32_t Type;       // +0x10
 };
 
 struct LoginOptionsInternal {
-    int32_t ApiVersion;   // +0x00
-    void*   Credentials;  // +0x08
-    void*   UserLoginInfo;// +0x10
+    int32_t ApiVersion;      // +0x00
+    uint32_t padding;        // +0x04
+    void* Credentials;       // +0x08
+    void* UserLoginInfo;     // +0x10
 };
