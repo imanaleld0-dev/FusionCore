bc25e812cb8aebcf53aac0c6f42028f20f7b6ccf
Twix234 <eldiarimanaliev220@gmail.com>
2026-09-04 15:30:13 +0600

    Create eos_hooks.h

---
+++ fusionApp/src/main/jni/fusion/include/hooking/eos_hooks.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+#include <logger.h>
+#include <external/dobby.h>
+#include <dlfcn.h>
+
+#define TAG "EOS_Hooks"
+
+void install_eos_hooks();
+
+struct CredentialsInternal {
+    int32_t ApiVersion;   // +0x00
+    void*   Token;        // +0x08
+    int32_t Type;         // +0x10
+};
+
+struct LoginOptionsInternal {
+    int32_t ApiVersion;   // +0x00
+    void*   Credentials;  // +0x08
+    void*   UserLoginInfo;// +0x10
+};
