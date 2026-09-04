631e594d478d6f9fe62e52f33dc85227d47e5a09
Twix234 <eldiarimanaliev220@gmail.com>
2026-09-05 04:46:44 +0600

    Update fusion.cpp

--- fusionApp/src/main/jni/fusion/src/fusion.cpp
+++ fusionApp/src/main/jni/fusion/src/fusion.cpp
@@ -32,9 +32,7 @@
 
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
-
-
+#define LOGW(...) __android_log_print(ANDROID_OG_WARN, TAG, __VA_ARGS__)
 
 static void* g_allocated_token = nullptr;
 static void* g_allocated_credentials = nullptr;
