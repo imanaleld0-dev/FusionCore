363dcf540a868c985217dab740e72d424f23585b
Twix234 <eldiarimanaliev220@gmail.com>
2026-09-05 10:07:06 +0600

    Update eos_hooks.h

--- fusionApp/src/main/jni/fusion/include/hooking/eos_hooks.h
+++ fusionApp/src/main/jni/fusion/include/hooking/eos_hooks.h
@@ -1,11 +1,23 @@
 #ifndef EOS_HOOKS_H
 #define EOS_HOOKS_H
-
+#include <stdint.h>
+#include <jni.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef int32_t (*EOS_Connect_Login_t)(
+    void* handle,           
+    void* options,
+    void* clientData,       
+    void* completionDelegate
+);
+
+
+bool InstallEOSConnectLoginHook();
+void RemoveEOSConnectLoginHook();
 void install_eos_hooks();
+void LogAuthDiag(const char* fmt, ...);
 
 #ifdef __cplusplus
 }
